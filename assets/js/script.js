f746563727b4f61c62294d53611186e5203a00a7
Jerome <jeromelesne@gmail.com>
2023-12-06 13:40:56 +0100

    work in progress

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,35 +1,68 @@
 // Create and resize image
 let clown = document.createElement("img");
-clown.src = "./assets/images/clown.png";
-clown.style.width = "150px";
-
 const div = document.querySelector("#main");
-div.appendChild(clown);
+const p = document.querySelector("p");
+const lifeElmt = document.querySelector("p > span");
+let moveInterval = 1300;
 
 let clownWidth = 0;
 let clownHeight = 0;
+let life = 0;
+
+lifeElmt.innerHTML = life;
+
+clown.src = "./assets/images/clown.png";
+clown.style.width = "150px";
+div.appendChild(clown);
+
 clown.onload = () => {
     clownWidth = clown.width;
     clownHeight = clown.height;
 };
 
-console.log(window.innerWidth);
-console.log(window.innerHeight);
-
 const getRandInt = (max) => {
     return Math.floor(Math.random() * max);
 };
 
-const randMove = (elmt) => {
-    let randomX = getRandInt(window.innerWidth - clownWidth);
-    let randomY = getRandInt(window.innerHeight - clownHeight);
-    // elmt.style.top = `${randomY}px`;
-    // elmt.style.left = `${randomX}px`;
-    elmt.style.transform = `translate(${randomX}px, ${randomY}px)`;
-    console.log("x :", randomX);
-    console.log("y :", randomY);
+const shake = (elmt) => {
+    elmt.classList.toggle("animShake");
+    console.log(elmt.outerHTML);
 };
 
+const randMove = (elmt) => {
+    let randomX = getRandInt(div.offsetWidth - clownWidth);
+    let randomY = getRandInt(div.offsetHeight - clownHeight);
+    elmt.style.transform = `translate(${randomX}px, ${randomY}px)`;
+    elmt.style.transition = `transform ease ${moveInterval}ms`;
+};
+const timer = (min = 0, sec) => {
+    const interval = setInterval(() => {
+        if (min > 0 && sec == 0) {
+            sec = 59;
+            min--;
+        }
+        if (sec < 10) {
+            console.log(min + ":0" + sec);
+        } else {
+            console.log(min + ":" + sec);
+        }
+        if (min == 0 && sec == 0) {
+            clearInterval(interval);
+        }
+        sec--;
+    }, 1000);
+};
+
+timer(0, 15);
+
 clown.addEventListener("click", () => {
-    randMove(clown);
+    shake(p);
+    life++;
+    lifeElmt.innerHTML = life;
 });
+p.addEventListener("animationend", () => {
+    shake(p);
+});
+setInterval(() => {
+    randMove(clown);
+}, moveInterval);

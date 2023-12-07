dd443ac646b1560b9e2e849a8a0c13fd7e1bd7d4
Jerome <jeromelesne@gmail.com>
2023-12-07 17:48:58 +0100

    work in progress

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,4 @@
-// Create and resize image
-let clown = document.createElement("img");
-const div = document.querySelector("#main");
-const p = document.querySelector("p");
-const lifeElmt = document.querySelector("p > span");
-const timerElmt = document.querySelector("#timerDigit");
-let finishMsg = document.createElement("div");
-
-let moveInterval = 1500;
+let moveInterval = 1000;
 let clownWidth = 0;
 let clownHeight = 0;
 let min = 0;
@@ -14,11 +6,32 @@ let sec = 0;
 let life = 50;
 let lifDecInt = 1000;
 
+let clown = document.createElement("img");
+const div = document.querySelector("#main");
+const p = document.querySelector("p");
+const lifeElmt = document.querySelector("p > span");
+const timerElmt = document.querySelector("#timerDigit");
+const finishMsg = document.createElement("div");
+const finishText = document.createElement("p");
+const finishTime = document.createElement("p");
+const resetBtn = document.createElement("button");
+const sideBar = document.querySelector("#sideBar");
+const burgerMenu = document.querySelector("#burgerMenu");
+const closeBtn = document.querySelector("#closeBtn");
+resetBtn.setAttribute("type", "submit");
+finishMsg.appendChild(finishText);
+finishMsg.appendChild(finishTime);
+finishMsg.appendChild(resetBtn);
+
+resetBtn.innerHTML = "Recommencer";
+resetBtn.style.width = "100px";
 lifeElmt.innerHTML = life;
+
 clown.src = "./assets/images/clown.png";
 clown.style.width = "150px";
 clown.style.filter = "drop-shadow(16px 16px 20px black)";
 clown.style.userSelect = "none";
+clown.setAttribute("draggable", "false");
 div.appendChild(clown);
 
 clown.onload = () => {
@@ -27,6 +40,21 @@ clown.onload = () => {
 };
 
 //Functions
+
+const openNav = () => {
+    sideBar.style.left = "0";
+    sideBar.style.transition = "left ease 1s";
+    burgerMenu.style.transform = "rotate(-90deg)";
+    burgerMenu.style.transition = "transform ease 0.5s";
+};
+
+const closeNav = () => {
+    sideBar.style.left = "-230px";
+    sideBar.style.transition = "left ease 1s";
+    burgerMenu.style.transform = "rotate(0deg)";
+    burgerMenu.style.transition = "transform ease 1.5s";
+};
+
 const getRandInt = (max) => {
     return Math.floor(Math.random() * max);
 };
@@ -39,17 +67,23 @@ const randMove = (elmt) => {
     let randomX = getRandInt(div.offsetWidth - clownWidth);
     let randomY = getRandInt(div.offsetHeight - clownHeight);
     elmt.style.transform = `translate(${randomX}px, ${randomY}px)`;
-    elmt.style.transition = `transform linear 1000ms`;
+    elmt.style.transition = `transform linear ${moveInterval}ms`;
 };
+
+const increaseSpeed = (sec) => {
+    interval = setInterval(() => {
+        intervalClown = setInterval(() => {
+            randMove(clown);
+        }, moveInterval);
+        moveInterval /= 1.1;
+    }, sec * 1000);
+};
+
 const timer = (elmt) => {
     const interval = setInterval(() => {
         if (sec > 59) {
             sec = 0;
             min++;
-            moveInterval /= 2;
-            intervalClown = setInterval(() => {
-                randMove(clown);
-            }, moveInterval);
         }
         if (sec < 10) {
             elmt.innerHTML = `${min}:0${sec}`;
@@ -73,14 +107,19 @@ const lifeDecrease = () => {
     });
 };
 
-let finish = async (interval) => {
-    timer(timerElmt);
+timer(timerElmt);
+let game = async (interval) => {
+    increaseSpeed(10);
     await lifeDecrease();
     clown.remove();
     clearInterval(interval);
     finishMsg.classList.add("floatingMsg");
     div.appendChild(finishMsg);
     console.log("Game Over");
+    const finalTime = document.querySelector("#timerDigit").innerHTML;
+    timerElmt.remove();
+    finishText.innerHTML = "Bravo vous avez tenu :";
+    finishTime.innerHTML = finalTime;
 };
 
 clown.addEventListener("click", () => {
@@ -96,5 +135,14 @@ p.addEventListener("animationend", () => {
 intervalClown = setInterval(() => {
     randMove(clown);
 }, moveInterval);
+resetBtn.addEventListener("click", () => {
+    window.location.reload();
+});
+burgerMenu.addEventListener("click", () => {
+    openNav();
+});
+closeBtn.addEventListener("click", () => {
+    closeNav();
+});
 
-finish(intervalClown);
+game(intervalClown);
